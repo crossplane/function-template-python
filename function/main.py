1e6b3e1722343bf84f26fc5e73d2d16f56dff302
Jesús Fernández <7312236+fernandezcuesta@users.noreply.github.com>
2026-09-11 23:39:23 +0200

    feat: leverage reusable cli standard options
    Signed-off-by: Jesús Fernández <7312236+fernandezcuesta@users.noreply.github.com>

--- function/main.py
+++ function/main.py
@@ -1,50 +1,16 @@
 """The composition function's main CLI."""
 
 import click
-from crossplane.function import logging, runtime
+from crossplane.function import cli as sdkcli
 
 from function import fn
 
 
 @click.command()
-@click.option(
-    "--debug",
-    "-d",
-    is_flag=True,
-    help="Emit debug logs.",
-)
-@click.option(
-    "--address",
-    default="0.0.0.0:9443",
-    show_default=True,
-    help="Address at which to listen for gRPC connections",
-)
-@click.option(
-    "--tls-certs-dir",
-    help="Serve using mTLS certificates.",
-    envvar="TLS_SERVER_CERTS_DIR",
-)
-@click.option(
-    "--insecure",
-    is_flag=True,
-    help="Run without mTLS credentials. "
-    "If you supply this flag --tls-certs-dir will be ignored.",
-)
-def cli(debug: bool, address: str, tls_certs_dir: str, insecure: bool) -> None:  # noqa:FBT001  # We only expect callers via the CLI.
+@sdkcli.standard_options
+def cli(**kwargs):
     """A Crossplane composition function."""
-    try:
-        level = logging.Level.INFO
-        if debug:
-            level = logging.Level.DEBUG
-        logging.configure(level=level)
-        runtime.serve(
-            fn.FunctionRunner(),
-            address,
-            creds=runtime.load_credentials(tls_certs_dir),
-            insecure=insecure,
-        )
-    except Exception as e:
-        click.echo(f"Cannot run function: {e}")
+    sdkcli.run(fn.FunctionRunner(), **kwargs)
 
 
 if __name__ == "__main__":
